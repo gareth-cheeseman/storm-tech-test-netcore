e46b72d9c890166e112f9ba4dd13550052aa7b12
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-11-11 20:31:55 +0000

    Adds ToDoList index check

---
+++ Todo.Tests/FieldFactoryTests/WhenTodoListIndexCreated.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.EntityModelMappers.TodoLists;
+using Todo.Models.TodoLists;
+using Todo.Tests.TodoListUtilities;
+using Xunit;
+
+namespace Todo.Tests.FieldFactoryTests
+{
+    public class WhenTodoListIndexCreated
+    {
+        private readonly IEnumerable<TodoList> srcTodoLists;
+        private readonly TodoListIndexViewmodel resultFields;
+
+        public WhenTodoListIndexCreated()
+        {
+            srcTodoLists = new List<TodoList>
+            {
+
+                new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                    .WithItem("bread", Importance.High)
+                    .WithItem("milk", Importance.High)
+                    .WithItem("cheese", Importance.Medium)
+                    .WithItem("lettuce", Importance.Low)
+                    .WithItem("tomato", Importance.Medium)
+                    .Build(),
+                new TestTodoListBuilder(new IdentityUser("alice@example.com"), "workshop")
+                    .WithItem("timber", Importance.High)
+                    .WithItem("saw", Importance.High)
+                    .WithItem("drill", Importance.Medium)
+                    .WithItem("bench", Importance.Low)
+                    .WithItem("vacuum", Importance.Medium)
+                    .Build()
+
+
+            };
+
+            resultFields = TodoListIndexViewmodelFactory.Create(srcTodoLists);
+        }
+
+
+        [Fact]
+        public void EqualItemsCount()
+        {
+            Assert.Equal(srcTodoLists.Count(), resultFields.Lists.Count);
+        }
+
+    }
+}
