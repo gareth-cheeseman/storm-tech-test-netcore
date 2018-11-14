89d22d6c66e7649e9ad75965fc4243105a93f226
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-11-14 16:43:54 +0000

    Adds Rank to TodoItem and adds migration

--- Todo/Data/Migrations/20181114164240_Adds_Rank_To_TodoItem.Designer.cs
+++ Todo/Data/Migrations/20181114164240_Adds_Rank_To_TodoItem.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todo.Data;
 
 namespace Todo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181114164240_Adds_Rank_To_TodoItem")]
+    partial class Adds_Rank_To_TodoItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
