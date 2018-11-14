f7f71e11ca5634cd37ba052470a6b12d7f1453e8
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-11-14 16:08:19 +0000

    Adds SingleTodoList and SingleTodoItem tests

---
+++ Todo.Tests/ServicesTests/WhenGettingSingleTodoItem.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.AspNetCore.Identity;
+using Shouldly;
+using Todo.Data.Entities;
+using Todo.Services;
+using Todo.Tests.TodoListUtilities;
+using Xunit;
+
+namespace Todo.Tests.ServicesTests
+{
+    public class WhenGettingSingleTodoItem : InMemoryDbTest
+    {
+        private TodoItem srcTodoItem { get; set; }
+        private TodoItem resultTodoItem { get; set; }
+
+        public WhenGettingSingleTodoItem()
+        {
+            var todoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                .WithItem("bread", Importance.High)
+                .Build();
+
+            srcTodoItem = todoList.Items.First();
+
+            WithContext(context =>
+            {
+                context.Add(srcTodoItem);
+                context.SaveChanges();
+            });
+
+            WithContext(context =>
+            {
+                resultTodoItem = context.SingleTodoItem(srcTodoItem.TodoItemId);
+            });
+        }
+
+        [Fact]
+        public void EqualTodoListId()
+        {
+            resultTodoItem.TodoListId.ShouldBe(srcTodoItem.TodoListId);
+
+        }
+
+        [Fact]
+        public void EqualTitle()
+        {
+            resultTodoItem.Title.ShouldBe(srcTodoItem.Title);
+        }
+
+        [Fact]
+        public void EqualImportance()
+        {
+            resultTodoItem.Importance.ShouldBe(srcTodoItem.Importance);
+        }
+
+
+
+    }
+}
---
+++ Todo.Tests/ServicesTests/WhenGettingSingleTodoList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.Services;
+using Todo.Tests.TodoListUtilities;
+using Xunit;
+using Shouldly;
+
+namespace Todo.Tests.ServicesTests
+{
+    public class WhenGettingSingleTodoList : InMemoryDbTest
+    {
+        private TodoList srcTodoList { get; set; }
+        private TodoList resultTodoList { get; set; }
+
+        public WhenGettingSingleTodoList()
+        {
+            srcTodoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                .WithItem("bread", Importance.High)
+                .WithItem("milk", Importance.High)
+                .WithItem("cheese", Importance.Medium)
+                .WithItem("lettuce", Importance.Low)
+                .WithItem("tomato", Importance.Medium)
+                .Build();
+
+            WithContext(context =>
+            {
+                context.Add(srcTodoList);
+                context.SaveChanges();
+            });
+
+            WithContext(context =>
+            {
+                resultTodoList = context.SingleTodoList(srcTodoList.TodoListId);
+            });
+        }
+
+        [Fact]
+        public void EqualTodoListId()
+        {
+           resultTodoList.TodoListId.ShouldBe(srcTodoList.TodoListId);
+
+        }
+
+        [Fact]
+        public void EqualTitle()
+        {
+            resultTodoList.Title.ShouldBe(srcTodoList.Title);
+        }
+
+        [Fact]
+        public void EqualItemsCount()
+        {
+            resultTodoList.Items.Count.ShouldBe(srcTodoList.Items.Count);
+        }
+
+
+
+    }
+}
