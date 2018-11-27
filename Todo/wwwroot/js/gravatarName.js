2628782f3fea788f71239af3166f9620db293afa
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-11-27 17:10:10 +0000

    Fixes names and image call  to only call gravatar api once for each unique responsible party.

--- Todo/wwwroot/js/gravatarName.js
+++ Todo/wwwroot/js/gravatarName.js
@@ -1,13 +1,38 @@
-const getGravatarName = (hash, id) => {    
-    
-    this.getName = res => {
-        
-        document.getElementById(id).innerHTML = res.entry[0].displayName;
-    }
-        var ggn = document.createElement('script'); 
-        ggn.type = 'text/javascript';
-        ggn.async 
-        ggn.src = `https://www.gravatar.com/${hash}.json?callback=getName`;
-        var ref = document.getElementById(id);
-        ref.parentNode.appendChild(ggn);    
+class Gravatar {
+  static getUniqueEmailHash() {
+    const elements = Array.from(document.querySelectorAll('span[data-hash]'));
+    const hashes = elements.map(e => e.getAttribute('data-hash'));
+    const uniqueHashes = [...new Set(hashes)];
+    return uniqueHashes;
+  }
+
+  static addNames(res) {
+    const profile = res.entry[0];
+    const elements = Array.from(
+      document.querySelectorAll(`span[data-hash='${profile.requestHash}']`)
+    );
+    elements.forEach(element => (element.innerHTML = profile.displayName));
+  }
+
+  static namesCall() {
+    this.getUniqueEmailHash().forEach(hash => {
+      const ggn = document.createElement('script');
+      ggn.type = 'text/javascript';
+      ggn.async;
+      ggn.src = `https://www.gravatar.com/${hash}.json?callback=Gravatar.addNames`;
+      document.body.append(ggn);
+    });
+  }
+
+  static imagesCall() {
+    this.getUniqueEmailHash().forEach(hash => {
+      const elements = Array.from(
+        document.querySelectorAll(`img[data-hash='${hash}']`)
+      );
+      elements.forEach(
+        element =>
+          (element.src = `https://www.gravatar.com/avatar/${hash}?s=30`)
+      );
+    });
+  }
 }
