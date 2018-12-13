4eab0297cba8d7dbb76ef61d7e72ed50e0888572
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-12-13 12:23:52 +0000

    Adds todo items api controller

---
+++ Todo/Controllers/TodoItemsApiController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+using Todo.Data.Entities;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItemsApi
+        [HttpGet]
+        public IEnumerable<TodoItem> GetTodoItems()
+        {
+            return _context.TodoItems;
+        }
+
+        // GET: api/TodoItemsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTodoItem([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todoItem);
+        }
+
+        // PUT: api/TodoItemsApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem([FromRoute] int id, [FromBody] TodoItem todoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todoItem.TodoItemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItemsApi
+        [HttpPost]
+        public async Task<IActionResult> PostTodoItem([FromBody] TodoItem todoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.TodoItemId }, todoItem);
+        }
+
+        // DELETE: api/TodoItemsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(todoItem);
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return _context.TodoItems.Any(e => e.TodoItemId == id);
+        }
+    }
+}
