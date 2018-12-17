36ae3afa2d336a96e52c0d21122872db8345f747
Gareth Cheeseman <gareth.lewis.cheeseman@gmail.com>
2018-12-17 22:40:16 +0100

    Adds check for correct list returned.

--- Todo/wwwroot/js/TodoListDetail.js
+++ Todo/wwwroot/js/TodoListDetail.js
@@ -1,25 +1,29 @@
 import { getJson } from './FetchService.js';
 import { todoSummary } from './TodoSummary.js';
 
-export const listTodos = url => {
-  getJson(url).then(todoList =>
-    todoList.items.forEach(todo => {
-      const { todoItemId, title, responsibleParty, importance, rank } = todo;
+export const detail = (url, todoListId) => {
+  getJson(url + todoListId).then(todoList => {
+    if (todoList.todoListId == todoListId) {
+      todoList.items.forEach(todo => {
+        const { todoItemId, title, responsibleParty, importance, rank } = todo;
 
-      const todoSummaryView = todoSummary(
-        todoItemId,
-        title,
-        responsibleParty,
-        importance,
-        rank
-      );
+        const todoSummaryView = todoSummary(
+          todoItemId,
+          title,
+          responsibleParty,
+          importance,
+          rank
+        );
 
-      const fragment = document
-        .createRange()
-        .createContextualFragment(todoSummaryView);
+        const fragment = document
+          .createRange()
+          .createContextualFragment(todoSummaryView);
 
-      const todoList = document.querySelector('#todos');
-      todoList.append(fragment);
-    })
-  );
+        const todoList = document.querySelector('#todos');
+        todoList.append(fragment);
+      });
+    } else {
+      console.log('Wrong list returned');
+    }
+  });
 };
