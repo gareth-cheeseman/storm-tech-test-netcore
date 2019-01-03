3f8376d146dc94b0e9fbe49d3bbe7683c7953306
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2019-01-03 12:53:54 +0000

    Moves event listeners to DetailBuilder file.

--- Todo/wwwroot/js/DetailBuilder.js
+++ Todo/wwwroot/js/DetailBuilder.js
@@ -20,7 +20,26 @@ export function todoListBuild(todoListItems) {
   getGravatarName();
 }
 
-window.orderByRank = function(status) {
+export function setOrderRankButton() {
+  let orderByRankButton = document.querySelector(`#orderByRank`);
+  orderByRankButton.addEventListener(
+    'click',
+    function(event) {
+      if (orderByRankButton.getAttribute('data-orderedByRank') == 'false') {
+        orderByRank('false');
+        event.target.textContent = 'Order by importance';
+        event.target.setAttribute('data-orderedByRank', 'true');
+      } else {
+        orderByRank('true');
+        event.target.textContent = 'Order by rank';
+        event.target.setAttribute('data-orderedByRank', 'false');
+      }
+    },
+    false
+  );
+}
+
+export function orderByRank(status) {
   const todoListId = document
     .querySelector('#todoListId')
     .getAttribute('data-todoListId');
@@ -33,4 +52,27 @@ window.orderByRank = function(status) {
   } else {
     todoListBuild(todoList.items);
   }
-};
+}
+
+export function setShowHiddenButton() {
+  let hideDoneButton = document.querySelector(`#hideDone`);
+  hideDoneButton.addEventListener(
+    'click',
+    function(event) {
+      if (hideDoneButton.getAttribute('data-hidden-done') == 'false') {
+        const elements = document.querySelectorAll('[data-isDone=true]');
+        elements.forEach(element =>
+          element.setAttribute('style', 'display:none')
+        );
+        event.target.textContent = 'Show done todos';
+        event.target.setAttribute('data-hidden-done', 'true');
+      } else {
+        const elements = document.querySelectorAll('[data-isDone=true]');
+        elements.forEach(element => element.removeAttribute('style'));
+        event.target.textContent = 'Hide done todos';
+        event.target.setAttribute('data-hidden-done', 'false');
+      }
+    },
+    false
+  );
+}
